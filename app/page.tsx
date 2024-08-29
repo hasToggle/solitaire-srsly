837d6d3526f44338009ea37f74b6eaadac2936aa
Staicy Stadtler <staicy@hotmail.de>
2024-08-29 15:32:20 +0200

    footer with three buttons, WIP

--- app/page.tsx
+++ app/page.tsx
@@ -23,67 +23,79 @@ export default function Home() {
   } = useGameState();
 
   return (
-    <main>
-      <div className="grid min-h-screen grid-cols-12 p-24">
-        <div className="col-span-6 col-start-2 grid grid-cols-4">
-          {goalCards.map((cardStack, cardIndex) => (
-            <Card
-              key={cardIndex}
-              card={cardStack[cardStack.length - 1]}
-              onClick={() =>
-                handleGoalSelection(
-                  cardIndex,
-                  Math.max(0, cardStack.length - 1),
-                )
-              }
-              className={"text-4xl"}
-            />
-          ))}
-        </div>
-        <div className="col-span-6 col-start-9 flex items-start justify-center gap-7">
-          <div className="mt-5">
-            {leftStack.length ? (
+    <>
+      <main>
+        <div className="grid min-h-screen grid-cols-12 p-24">
+          <div className="col-span-6 col-start-2 grid grid-cols-4">
+            {goalCards.map((cardStack, cardIndex) => (
               <Card
-                card={leftStack[leftStack.length - 1]}
-                onClick={() => handleDrawSelection()}
-                onDoubleClick={handleDrawSendToGoal}
+                key={cardIndex}
+                card={cardStack[cardStack.length - 1]}
+                onClick={() =>
+                  handleGoalSelection(
+                    cardIndex,
+                    Math.max(0, cardStack.length - 1),
+                  )
+                }
                 className={"text-4xl"}
               />
-            ) : (
-              <EmptySlot />
-            )}
+            ))}
           </div>
-          <div className="">
-            <Card card={rightStack[0]} onClick={handleDrawCard} />
-          </div>
-        </div>
-        <div className="col-span-6 col-start-3 grid grid-cols-7 gap-2">
-          {mainCards.map((cardStack, stackIndex) => (
-            <Stack key={stackIndex}>
-              {cardStack.length === 0 && (
-                <EmptySlot onClick={() => handleMainSelection(stackIndex, 0)} />
-              )}
-              {cardStack.map((card, cardIndex) => (
+          <div className="col-span-6 col-start-9 flex items-start justify-center gap-7">
+            <div className="mt-5">
+              {leftStack.length ? (
                 <Card
-                  card={card}
-                  key={cardIndex}
-                  onClick={() => handleMainSelection(stackIndex, cardIndex)}
-                  onDoubleClick={() =>
-                    handleMainSendToGoal(stackIndex, cardIndex)
-                  }
-                  className={"items-baseline text-xl"}
+                  card={leftStack[leftStack.length - 1]}
+                  onClick={() => handleDrawSelection()}
+                  onDoubleClick={handleDrawSendToGoal}
+                  className={"text-4xl"}
                 />
-              ))}
-            </Stack>
-          ))}
+              ) : (
+                <EmptySlot />
+              )}
+            </div>
+            <div className="">
+              <Card card={rightStack[0]} onClick={handleDrawCard} />
+            </div>
+          </div>
+          <div className="col-span-6 col-start-3 grid grid-cols-7 gap-2">
+            {mainCards.map((cardStack, stackIndex) => (
+              <Stack key={stackIndex}>
+                {cardStack.length === 0 && (
+                  <EmptySlot
+                    onClick={() => handleMainSelection(stackIndex, 0)}
+                  />
+                )}
+                {cardStack.map((card, cardIndex) => (
+                  <Card
+                    card={card}
+                    key={cardIndex}
+                    onClick={() => handleMainSelection(stackIndex, cardIndex)}
+                    onDoubleClick={() =>
+                      handleMainSendToGoal(stackIndex, cardIndex)
+                    }
+                    className={"items-baseline text-xl"}
+                  />
+                ))}
+              </Stack>
+            ))}
+          </div>
         </div>
-      </div>
-      <button
-        className="absolute bottom-5 left-1/2 -ml-4 rounded-md border border-green-300 px-8 py-1.5 text-green-950"
-        onClick={handleGameReset}
-      >
-        Reset
-      </button>
-    </main>
+      </main>
+      <footer className="flex justify-center gap-x-6 p-6">
+        <button className="rounded-md border border-green-300 px-8 py-1.5 text-green-950 transition ease-in-out hover:bg-white">
+          Rewind 🔙
+        </button>
+        <button
+          className="rounded-md border border-green-300 px-8 py-1.5 text-green-950 transition ease-in-out hover:bg-white"
+          onClick={handleGameReset}
+        >
+          Reset Game 🚨
+        </button>
+        <button className="rounded-md border border-green-300 px-8 py-1.5 text-green-950 transition ease-in-out hover:bg-white">
+          Redo 🔜
+        </button>
+      </footer>
+    </>
   );
 }
